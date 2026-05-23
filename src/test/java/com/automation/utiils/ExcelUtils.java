9cfcc8c9c9973add5dc4993aaef74c8495c57607
Andrei <gomboev.andrew.com>
2026-05-23 10:09:24 -0700

    Excel Utils Chanched

--- src/test/java/com/automation/utiils/ExcelUtils.java
+++ src/test/java/com/automation/utiils/ExcelUtils.java
@@ -1,15 +1,5 @@
 package com.automation.utiils;
 
 public class ExcelUtils {
-    // This is purposely left blank
-
-    // Svetlana's update
-
-    // 600k a year Letsgoo
-
-    // git push
-    // git pull = fetch + merge
-
-
-    // 100 years
+   // I am in develop branch,
 }
