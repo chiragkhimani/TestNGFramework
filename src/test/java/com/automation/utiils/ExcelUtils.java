59befa7eb04012dae3f73a8883937e785482ed7a
nomars787 <masterbig979@gmail.com>
2026-05-23 10:13:16 -0700

    My change

--- src/test/java/com/automation/utiils/ExcelUtils.java
+++ src/test/java/com/automation/utiils/ExcelUtils.java
@@ -1,15 +1,7 @@
 package com.automation.utiils;
 
 public class ExcelUtils {
-    // This is purposely left blank
-
-    // Svetlana's update
-
-    // 600k a year Letsgoo
-
-    // git push
-    // git pull = fetch + merge
+    // This develop branch
 
 
-    // 100 years
 }
