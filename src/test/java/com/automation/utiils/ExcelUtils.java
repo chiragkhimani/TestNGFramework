c4b7469a54cc9438bf8c2030b12e1342ce16c168
chiragkhimani <khimanichirag@gmail.com>
2026-05-23 22:36:50 +0530

    Excel Utils Changed

--- src/test/java/com/automation/utiils/ExcelUtils.java
+++ src/test/java/com/automation/utiils/ExcelUtils.java
@@ -1,15 +1,6 @@
 package com.automation.utiils;
 
 public class ExcelUtils {
-    // This is purposely left blank
+    // I am in develop branch, and I am making 600k per year
 
-    // Svetlana's update
-
-    // 600k a year Letsgoo
-
-    // git push
-    // git pull = fetch + merge
-
-
-    // 100 years
 }
